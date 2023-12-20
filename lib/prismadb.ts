cb34c583866fb7081bb6f9c370f70882eac893be
coyaSONG <qawsy12@gmail.com>
2023-12-20 19:48:03 +0900

    fix(db-setting): progress mongodb setting in prisma

---
+++ lib/prismadb.ts
@@ -0,0 +1,6 @@
+import { PrismaClient } from "@prisma/client";
+
+const client = global.prismadb || new PrismaClient();
+if (process.env.NODE_DEV === "production") global.prismadb = client;
+
+export default client;
