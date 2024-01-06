7cb4a4c4cd9aac209750dcc068423728484eb6a1
coyaSONG <qawsy12@gmail.com>
2024-01-06 19:11:47 +0900

    feat(all): wip sign in

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,7 +1,14 @@
 import { useState, useCallback } from "react";
+import axios from "axios";
 import Input from "@/components/Input";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/router";
+
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
 
 const Auth = () => {
+  const router = useRouter();
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
@@ -13,6 +20,34 @@ const Auth = () => {
       currentVariant === "login" ? "Register" : "login",
     );
   }, []);
+
+  const login = useCallback(async () => {
+    try {
+      await signIn("credentials", {
+        email,
+        password,
+        redirect: false,
+      });
+
+      router.push("/");
+    } catch (err) {
+      console.log(err);
+    }
+  }, [email, password, router]);
+
+  const register = useCallback(async () => {
+    try {
+      await axios.post("/api/register", {
+        email,
+        password,
+        name,
+      });
+
+      login();
+    } catch (err) {
+      console.log(err);
+    }
+  }, [email, password, name, login]);
 
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -54,9 +89,27 @@ const Auth = () => {
                 type="password"
                 value={password}
               />
-              <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+              <button
+                onClick={variant === "login" ? login : register}
+                className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition"
+              >
                 {variant === "login" ? "Login" : "Register"}
               </button>
+              <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+                <div
+                  onClick={() => signIn("github", { callbackUrl: "/" })}
+                  className="w-10 h-10 bg-white rounded-full flex justify-center items-center cursor-pointer hover:opacity-80 transition"
+                >
+                  <FcGoogle size={30} />
+                </div>
+
+                <div
+                  onClick={() => signIn("github", { callbackUrl: "/" })}
+                  className="w-10 h-10 bg-white rounded-full flex justify-center items-center cursor-pointer hover:opacity-80 transition"
+                >
+                  <FaGithub size={30} />
+                </div>
+              </div>
               <p className="text-neutral-500 mt-12">
                 {variant === "login"
                   ? "First time Netflix?"
